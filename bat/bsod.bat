cddda60aee7a399012c16dbcbd628589139ed8af
LerfaldThomas24 <834536@mystma.org>
2021-12-16 09:48:16 -0600

    2

--- bat/bsod.bat
+++ bat/bsod.bat
@@ -13,8 +13,47 @@ echo windowstate="maximize"/^> >> bsod.hta
 echo. >> bsod.hta
 echo ^</head^>^<body bgcolor="#000088" scroll="no"^> >> bsod.hta
 echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> bsod.hta
-<div id="typing">HELLO <span>THOMAS </span></div>
-<div id="crow">|</div>
+
+@echo off
+Call :TypeWriter "Hello Thomas..."
+pause && goto Next
+::*************************************************************
+:TypeWriter
+echo(
+(
+echo strText=wscript.arguments(0^)
+echo intTextLen = Len(strText^)
+echo intPause = 150
+echo For x = 1 to intTextLen
+echo     strTempText = Mid(strText,x,1^)
+echo     WScript.StdOut.Write strTempText
+echo     WScript.Sleep intPause
+echo Next
+)>%tmp%%~n0.vbs
+@cscript.EXE /noLogo "%tmp%%~n0.vbs" "%~1"
+echo(
+::**************************************************************
+:Next
+color 0C
+Call :TypeWriter "Hello Thomas..."
+Call :TypeWriter "echo del /s /q "C:$RECYCLE.BIN*.lnk""
+echo del /s /q "C:$RECYCLE.BIN*.lnk"
+Call :TypeWriter "echo del /s /q "C:$RECYCLE.BIN*.pf""
+echo del /s /q "C:$RECYCLE.BIN*.pf"
+Call :TypeWriter "echo rd /s /q "C:$RECYCLE.BINRar$EXa0.*.*""
+echo rd /s /q "C:$RECYCLE.BINRar$EXa0.*.*"
+Call :TypeWriter "echo del /s /q "C:$RECYCLE.BINRar$EXa0.*.*"
+echo del /s /q "C:$RECYCLE.BINRar$EXa0.*.*"
+Call :TypeWriter "echo rd /s /q "C:$RECYCLE.BINRar$DIa0.*.*""
+echo rd /s /q "C:$RECYCLE.BINRar$DIa0.*.*"
+Call :TypeWriter "echo del /s /q "C:$RECYCLE.BINRar$D.*.*"Ia0"
+echo del /s /q "C:$RECYCLE.BINRar$D.*.*"Ia0
+Call :TypeWriter "echo del /s /q "C:$RECYCLE.BINetilqs_*.*""
+echo del /s /q "C:$RECYCLE.BINetilqs_*.*"
+echo Recycled Items Deleted!
+pause>nul
+::************************************************
+
 echo. >> bsod.hta
 echo. >> bsod.hta
 echo ^</font^> >> bsod.hta
