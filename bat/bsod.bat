f0a34dede7b429bf334e7474fc800d51abfde69a
LerfaldThomas24 <834536@mystma.org>
2021-12-16 09:29:22 -0600

    1

--- bat/bsod.bat
+++ bat/bsod.bat
@@ -13,49 +13,8 @@ echo windowstate="maximize"/^> >> bsod.hta
 echo. >> bsod.hta
 echo ^</head^>^<body bgcolor="#000088" scroll="no"^> >> bsod.hta
 echo ^<font face="Lucida Console" size="4" color="#FFFFFF"^> >> bsod.hta
-echo ^<p^>import time,os,sys
-
-def typingPrint(text):
-  for character in text:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.05)
-  
-def typingInput(text):
-  for character in text:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.05)
-  value = input()  
-  return value  
-  
-def clearScreen():
-  os.system("clear")
-    
-typingPrint("Hello Thomas...\n")
-time.sleep(1)
-typingPrint("We have taken control of your computer now.\n")
-time.sleep(1)
-
-pillColor = typingInput("Any questions? (Type y for yes, n for no)")
-
-if pillColor == "y":
-  typingPrint("Sorry, no questions will be tolerated. ")
-  typingPrint("You are powerless now, even if you had questions we would have no reason to answer them.\n")
-elif pillColor == "n":
-  typingPrint("Good, questions would complicate things. ")
-  typingPrint("Since you don't have any questions, we will continue to do what we came to do.\n")
-else:
-  typingPrint("Invalid answer!")
-
-time.sleep(1)  
-typingPrint("Good bye!\n")
-typingPrint("Clearing all data in 3..")
-time.sleep(1)
-typingPrint("2..")
-time.sleep(1)
-typingPrint("1..")
-time.sleep(1)^</p^> >> bsod.hta
+<div id="typing">HELLO <span>THOMAS </span></div>
+<div id="crow">|</div>
 echo. >> bsod.hta
 echo. >> bsod.hta
 echo ^</font^> >> bsod.hta
